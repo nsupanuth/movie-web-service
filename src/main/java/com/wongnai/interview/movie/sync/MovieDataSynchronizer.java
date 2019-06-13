159d897addde27e558a53e9c43eea1235777cb1c
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2019-06-13 21:49:34 +0700

    Implement force sync method

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,17 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MoviesResponse;
+import com.wongnai.interview.util.MovieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +25,12 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+		List<Movie> movies = moviesResponse
+				.stream()
+				.map(MovieUtils::mapMovieDataToMovie)
+				.collect(Collectors.toList());
+
+		movieRepository.saveAll(movies);
 	}
 }
