16cc98b817f3e35058f3e39358837f825d832442
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2019-06-13 21:33:02 +0700

    Retrieve data from movie data base

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,17 @@
 package com.wongnai.interview.movie.external;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -24,6 +31,16 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
 
-		return null;
+		MoviesResponse moviesResponse = null;
+		try {
+			ResponseEntity<String> result = restTemplate.getForEntity(MOVIE_DATA_URL, String.class);
+			moviesResponse = objectMapper.readValue(result.getBody(), MoviesResponse.class);
+		} catch (JsonParseException | JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return moviesResponse;
 	}
 }
