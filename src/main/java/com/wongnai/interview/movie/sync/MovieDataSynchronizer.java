6ebc0c2f27f82e7ba80398fab6ff1ea7b43d05cf
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2019-06-15 11:18:03 +0700

    Fix run test all failed

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -1,36 +1,36 @@
 package com.wongnai.interview.movie.sync;
 
-import javax.transaction.Transactional;
-
 import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.MovieRepository;
+import com.wongnai.interview.movie.external.MovieDataService;
 import com.wongnai.interview.movie.external.MoviesResponse;
 import com.wongnai.interview.util.MovieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.wongnai.interview.movie.MovieRepository;
-import com.wongnai.interview.movie.external.MovieDataService;
-
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class MovieDataSynchronizer {
-	@Autowired
-	private MovieDataService movieDataService;
+    @Autowired
+    private MovieDataService movieDataService;
 
-	@Autowired
-	private MovieRepository movieRepository;
+    @Autowired
+    private MovieRepository movieRepository;
 
-	@Transactional
-	public void forceSync() {
-		//TODO: implement this to sync movie into repository
-		MoviesResponse moviesResponse = movieDataService.fetchAll();
-		List<Movie> movies = moviesResponse
-				.stream()
-				.map(MovieUtils::mapMovieDataToMovie)
-				.collect(Collectors.toList());
+    @Transactional
+    public void forceSync() {
+        //TODO: implement this to sync movie into repository
+        if (movieRepository.findAll().size() == 0) {
+            MoviesResponse moviesResponse = movieDataService.fetchAll();
+            List<Movie> movies = moviesResponse
+                    .stream()
+                    .map(MovieUtils::mapMovieDataToMovie)
+                    .collect(Collectors.toList());
 
-		movieRepository.saveAll(movies);
-	}
+            movieRepository.saveAll(movies);
+        }
+    }
 }
