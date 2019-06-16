7dfa2f2c45945043ad12e241d7c8786b5dc3b78c
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2019-06-16 09:05:20 +0700

    Add default case for any exception

---
+++ src/main/java/com/wongnai/interview/movie/exception/DefaultError.java
@@ -0,0 +1,43 @@
+package com.wongnai.interview.movie.exception;
+
+import java.util.Date;
+
+public class DefaultError {
+
+    private Date timestamp;
+    private int status;
+    private String error;
+    private String message;
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/com/wongnai/interview/movie/exception/ExceptionTranslator.java
+++ src/main/java/com/wongnai/interview/movie/exception/ExceptionTranslator.java
@@ -1,5 +1,6 @@
 package com.wongnai.interview.movie.exception;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +14,23 @@ import java.util.Date;
 @ControllerAdvice
 public class ExceptionTranslator {
 
+    private static final String DEFAULT_MESSAGE = "Please contact administrator";
+
+    @ExceptionHandler
+    public ResponseEntity<?> createException(Exception ex, WebRequest request) {
+        if (ex.getCause() instanceof InternalServerErrorException) {
+            handleInternalServerError((InternalServerErrorException) ex, request);
+        }
+
+        return handleDefaultError(ex, request);
+    }
+
     @ExceptionHandler({InternalServerErrorException.class})
-    public ResponseEntity<ApiError> handleInternalServerError(InternalServerErrorException ex, WebRequest request){
+    public ResponseEntity<ApiError> handleInternalServerError(InternalServerErrorException ex, WebRequest request) {
         HttpHeaders headers = new HttpHeaders();
         request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
 
-        ApiError body =  new ApiError(
+        ApiError body = new ApiError(
             new Date(),
             ex.getEntityName(),
             HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -31,10 +43,10 @@ public class ExceptionTranslator {
     }
 
     @ExceptionHandler({BadRequestAlertException.class})
-    public ResponseEntity<ApiError> handleBadRequest(BadRequestAlertException ex, WebRequest request){
+    public ResponseEntity<ApiError> handleBadRequest(BadRequestAlertException ex, WebRequest request) {
         HttpHeaders headers = new HttpHeaders();
         request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
-        ApiError body =  new ApiError();
+        ApiError body = new ApiError();
 
         body.setTimestamp(new Date());
         body.setEntityName(ex.getEntityName());
@@ -45,5 +57,19 @@ public class ExceptionTranslator {
 
         return new ResponseEntity<>(body, headers, HttpStatus.BAD_REQUEST);
     }
+
+    public ResponseEntity<DefaultError> handleDefaultError(Exception ex, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+        DefaultError body = new DefaultError();
+
+        body.setTimestamp(new Date());
+        body.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        body.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        body.setMessage(StringUtils.isNotBlank(ex.getMessage()) ? ex.getMessage() : DEFAULT_MESSAGE);
+
+        return new ResponseEntity<>(body, headers, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 
 }
